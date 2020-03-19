51179c3cc891742452403425bdcd0bfb4330ff32
TAY TS <taytzushieh@gmail.com>
2020-03-19 14:28:00 +0800

    Update proto generation script and format metadata proto file

--- generate.sh
+++ generate.sh
@@ -2,3 +2,4 @@
 
 protoc proto/recordpb/record.proto --go_out=plugins=grpc:..
 protoc proto/messagepb/message.proto --go_out=plugins=grpc:..
+protoc proto/metadatapb/metadata.proto --go_out=plugins=grpc:..
