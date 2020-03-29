24e23b770398b9f3759681b93d2d66fc09c4de72
TAY TS <taytzushieh@gmail.com>
2020-03-29 23:08:41 +0800

    Update AdminClientNewPartition to handle partition creation

--- broker/requests_handler.go
+++ broker/requests_handler.go
@@ -1,6 +1,7 @@
 package broker
 
 import (
+	"AKFAK/broker/partition"
 	"AKFAK/proto/adminpb"
 	"context"
 )
@@ -21,13 +22,29 @@ func (n *Node) AdminClientNewTopic(ctx context.Context, req *adminpb.AdminClient
 			Response: adminpb.Response_FAIL}, nil
 	}
 
+	partitionLeader := make(map[int]int)
+	partitionISR := make(map[int][]int)
+
 	for brokerID, req := range newPartitionReqMap {
 		res, err := n.peerCon[brokerID].AdminClientNewPartition(context.Background(), req)
-		if err != nil || res.GetResponse() == adminpb.Response_FAIL {
+		if err != nil && res.GetResponse() == adminpb.Response_FAIL {
 			// Terminate the partition creation
 			// TODO: Clean up partition if the process does not complete fully (nobody care in this school project anyway)
 			return &adminpb.AdminClientNewTopicResponse{
-				Response: adminpb.Response_FAIL}, nil
+				Response: adminpb.Response_FAIL}, err
+		}
+
+		for _, partID := range req.GetPartitionID() {
+			partIDInt := int(partID)
+			if _, exist := partitionLeader[partIDInt]; !exist {
+				partitionLeader[partIDInt] = brokerID
+				continue
+			}
+			if _, exist := partitionISR[partIDInt]; !exist {
+				partitionISR[partIDInt] = []int{brokerID}
+				continue
+			}
+			partitionISR[partIDInt] = append(partitionISR[partIDInt], brokerID)
 		}
 	}
 
@@ -38,5 +55,15 @@ func (n *Node) AdminClientNewTopic(ctx context.Context, req *adminpb.AdminClient
 
 // AdminClientNewPartition create new partition
 func (*Node) AdminClientNewPartition(ctx context.Context, req *adminpb.AdminClientNewPartitionRequest) (*adminpb.AdminClientNewPartitionResponse, error) {
+	topicName := req.GetTopic()
+	partitionID := req.GetPartitionID()
 
+	for _, partID := range partitionID {
+		err := partition.CreatePartitionDir(".", topicName, int(partID))
+		if err != nil {
+			return &adminpb.AdminClientNewPartitionResponse{Response: adminpb.Response_FAIL}, err
+		}
+	}
+
+	return &adminpb.AdminClientNewPartitionResponse{Response: adminpb.Response_SUCCESS}, nil
 }
