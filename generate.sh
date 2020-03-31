40c3908635abe5b81efdf3aa97608a78dd7805aa
TAY TS <taytzushieh@gmail.com>
2020-03-31 09:02:44 +0800

    Refactor out Response enum from admin proto

--- generate.sh
+++ generate.sh
@@ -5,3 +5,4 @@ protoc proto/messagepb/message.proto --go_out=plugins=grpc:..
 protoc proto/metadatapb/metadata.proto --go_out=plugins=grpc:..
 protoc proto/adminpb/admin.proto --go_out=plugins=grpc:..
 protoc proto/clientpb/client.proto --go_out=plugins=grpc:..
+protoc proto/commonpb/*.proto --go_out=plugins=grpc:..
