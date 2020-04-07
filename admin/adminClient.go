2878294db891ead80d61293bf88e0c71ba658c12
Huan An Cheng <huanan_cheng@mymail.sutd.edu.sg>
2020-04-07 14:51:13 +0800

    calling unimplemented methods of getmetadata

--- admin/adminClient.go
+++ admin/adminClient.go
@@ -2,6 +2,7 @@ package admin
 
 import (
 	"AKFAK/proto/adminpb"
+	"AKFAK/proto/adminclientpb"
 	"context"
 	"fmt"
 	"log"
@@ -19,21 +20,33 @@ func CreateNew() {
 	fmt.Println("Command", command)
 
 	// TODO: Initialise the gRPC connection to the Kafka server
-
+	opts := grpc.WithInsecure()
+	conn, err := grpc.Dial(command.KafkaServer, opts)
+	if err != nil {
+		log.Fatalf("Unable to connect to Kafka server %s: %v", command.KafkaServer, err)
+	}
+	adminClient := adminpb.NewAdminServiceClient(conn)
+	log.Println(adminClient)
+	
 	// TODO: Fetch the metadata for cluster
+	metadata, err := adminClient.GetMetadata(context.Background(), &adminclientpb.GetMetadataRequest{})
+	if err != nil {
+		log.Fatal("Failed to send getmetadata request to Kafka server")
+	}
 
 	// Initialise the gRPC connection with the Kafka controller
-	fmt.Println("Connecting to one of the Kafka server")
+	fmt.Printf("Connecting to controller: %d\n", metadata.GetControllerID())
 
-	// TODO: Get the controller host and port
-	gCon, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	// TODO: Get the controller host and port based on metadata.GetControllerID()
+	// gCon, err := grpc.Dial(controller, opts)
+	gCon, err := grpc.Dial("localhost:5001", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Unable to connect to Kafka server: %v\n", err)
 	}
 
 	adminCon := adminpb.NewAdminServiceClient(gCon)
 
-	req := &adminpb.AdminClientNewTopicRequest{
+	req := &adminclientpb.AdminClientNewTopicRequest{
 		Topic:             command.Topic,
 		NumPartitions:     int32(command.Partitions),
 		ReplicationFactor: int32(command.ReplicaFactor),
@@ -42,7 +55,7 @@ func CreateNew() {
 	// Send the topic operation request
 	res, err := adminCon.AdminClientNewTopic(context.Background(), req)
 	if err != nil {
-		log.Fatalf("Fail to sending the operation request to Kafka controller")
+		log.Fatalf("Fail to send operation request to Kafka controller: %v", err)
 	}
 	log.Printf("Response from Kafka controller: %v\n", res.GetResponse())
 }
---
+++ testing/test_create_new_topic.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"AKFAK/admin"
+)
+
+func main() {
+	admin.CreateNew()
+}
