2daa41a854b758546afdca13f7156bac72ac9a5e
TAY TS <taytzushieh@gmail.com>
2020-04-09 00:07:03 +0800

    Add broker CLI

---
+++ cmd/broker/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"AKFAK/broker"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	// get the user input for initialise the Broker
+	// TODO: Read the broker from config file from the given path
+	// flag.String(
+	// 	"-server-config",
+	// 	"",
+	// 	"Config to setup the Kafka broker")
+	brkIDPtr := flag.Int(
+		"-brokerID",
+		0,
+		"Broker unique ID")
+	hostPtr := flag.String(
+		"-host",
+		"0.0.0.0",
+		"Address for the broker to listen for connection")
+	portPtr := flag.Int(
+		"-port",
+		5000,
+		"Port for the broker to listen for connection")
+
+	// print usage if user does not provide sufficient infomation to start the broker
+	if len(os.Args) < 6 {
+		fmt.Println("usage: broker -brokerID <broker_ID> -host <ip_address> -port <port_number>")
+		os.Exit(2)
+	}
+	flag.Parse()
+
+	// start the broker
+	node := broker.InitNode(*brkIDPtr, *hostPtr, *portPtr)
+	go node.InitAdminListener()
+
+	// Wait for Ctrl-C to exit
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	// Block until a signal is received
+	<-ch
+}
