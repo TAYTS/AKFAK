2e44185400aef7c7caa20d4d64a5f5113e977739
Phangster <mynamebryanphang@gmail.com>
2020-04-12 18:17:25 +0800

    edited consumer

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -1,71 +1,28 @@
-package main
+package consumer
 
-import (
-    "fmt"
-    "log"
-    "os"
-    "time"
-    "consumer/common"
-	"consumer/consumergroup"
-	"consumer/config"
-)
-
-// const (
-//     zookeeperConn = "10.4.1.29:2181"
-//     cgroup = "zgroup"
-//     topic = "senz"
-// )
-
-func main() {
-    // setup sarama log to stdout
-    sarama.Logger = log.New(os.Stdout, "", log.Ltime)
-
-    // init consumer
-    cg, err := initConsumer()
-    if err != nil {
-        fmt.Println("Error consumer goup: ", err.Error())
-        os.Exit(1)
-    }
-    defer cg.Close()
-
-    // run consumer
-    consume(cg)
+type Consumer struct {
+	topic     string
+	partition int
+	broker    int
 }
 
-func initConsumer()(*consumergroup.ConsumerGroup, error) {
-    // consumer config
-    config := consumergroup.NewConfig()
-    config.Offsets.Initial = sarama.OffsetOldest
-    config.Offsets.ProcessingTimeout = 10 * time.Second
-
-    // join to consumer group
-    cg, err := consumergroup.JoinConsumerGroup(cgroup, []string{topic}, []string{zookeeperConn}, config)
-    if err != nil {
-        return nil, err
-    }
-
-    return cg, err
+func InitConsumer(topicName string, partitionIdx int, brokerIdx int) *Consumer {
+	return &Consumer{
+		topic:     topicName,
+		partition: partitionIdx,
+		broker:    brokerIdx,
+	}
 }
 
-func consume(cg *consumergroup.ConsumerGroup) {
-    for {
-        select {
-        case msg := <-cg.Messages():
-            // messages coming through chanel
-            // only take messages from subscribed topic
-	    if msg.Topic != topic {
-                continue
-            }
+// Get requests
+func (p *Consumer) GetTopic() string {
+	return p.topic
+}
 
-            fmt.Println("Topic: ", msg.Topic)
-            fmt.Println("Value: ", string(msg.Value))
+func (p *Consumer) GetPartitionID() int {
+	return p.partition
+}
 
-            // commit to zookeeper that message is read
-            // this prevent read message multiple times after restart
-            err := cg.CommitUpto(msg)
-            if err != nil {
-                fmt.Println("Error commit zookeeper: ", err.Error())
-            }
-        }
-    }
+func (p *Consumer) GetBroker() int {
+	return p.broker
 }
