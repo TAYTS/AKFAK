31e1ad7afb2a1ef184e78a53d9b1a3b8f6fdc311
TAY TS <taytzushieh@gmail.com>
2020-04-13 21:52:25 +0800

    Update broker node to call ZK to get the cluster metadata

--- broker/node.go
+++ broker/node.go
@@ -3,6 +3,8 @@ package broker
 import (
 	"AKFAK/proto/adminpb"
 	"AKFAK/proto/clientpb"
+	"AKFAK/proto/zkmessagepb"
+	"AKFAK/proto/zookeeperpb"
 	"context"
 	"fmt"
 	"log"
@@ -17,7 +19,7 @@ type Node struct {
 	ID                  int
 	Host                string
 	Port                int
-	Metadata            []int // Get the cluster metadata
+	Metadata            *zkmessagepb.MetadataCluster
 	adminServiceClient  map[int]adminpb.AdminServiceClient
 	clientServiceClient map[int]clientpb.ClientServiceClient
 }
@@ -54,11 +56,17 @@ func (n *Node) InitAdminListener() {
 	opts := []grpc.ServerOption{}
 	server := grpc.NewServer(opts...)
 
+	// bind the rpc server service
 	adminpb.RegisterAdminServiceServer(server, n)
 	clientpb.RegisterClientServiceServer(server, n)
 
-	// TODO: call ZK to get the update metadata
-	// TODO: initialise the internal state cache
+	// setup the cluster metadata cache
+	n.InitClusterMetadataCache()
+
+	// start controller routine if the broker is select as the controller
+	if int(n.Metadata.GetController().GetID()) == n.ID {
+		go n.InitControllerRoutine()
+	}
 
 	// setup ClientService peer connection
 	go n.EstablishClientServicePeerConn()
@@ -114,4 +122,36 @@ func (n *Node) EstablishClientServicePeerConn() {
 			cancel()
 		}
 	}
+}
+
+// InitClusterMetadataCache call the ZK to get the Cluster Metadata
+func (n *Node) InitClusterMetadataCache() {
+	// set up grpc dial
+	opts := grpc.WithInsecure()
+	zkAddress := "0.0.0.0:9092"
+	zkCon, err := grpc.Dial(zkAddress, opts)
+	if err != nil {
+		log.Fatalf("Fail to connect to %v: %v\n", zkAddress, err)
+	}
+
+	// set rpc client
+	zkClient := zookeeperpb.NewZookeeperServiceClient(zkCon)
+
+	// create request with the current broker info
+	req := &zkmessagepb.GetClusterMetadataRequest{
+		Broker: &zkmessagepb.MetadataBroker{
+			ID:   int32(n.ID),
+			Host: n.Host,
+			Port: int32(n.Port),
+		},
+	}
+
+	// send the GetClusterMetadata request
+	res, err := zkClient.GetClusterMetadata(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Fail to get the cluster metada from Zk")
+	}
+
+	// store the cluster metadata to cache
+	n.Metadata = res.GetClusterInfo()
 }
--- zookeeper/zookeeper_request_handlers.go
+++ zookeeper/zookeeper_request_handlers.go
@@ -13,6 +13,19 @@ func (zk *Zookeeper) GetClusterMetadata(ctx context.Context, req *zkmessagepb.Ge
 		// set the requesting broker as the controller
 		zk.clusterMetadata.Controller = req.GetBroker()
 	}
+
+	// add the requesting broker to live brokers
+	exist := false
+	reqBrk := req.GetBroker()
+	for _, brk := range zk.clusterMetadata.GetLiveBrokers() {
+		if brk.ID == reqBrk.GetID() && brk.Host == reqBrk.GetHost() && brk.Port == reqBrk.GetPort() {
+			exist = true
+			break
+		}
+	}
+	if !exist {
+		zk.clusterMetadata.LiveBrokers = append(zk.clusterMetadata.LiveBrokers, reqBrk)
+	}
 	zk.mux.Unlock()
 
 	// return response
