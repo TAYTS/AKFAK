eeddead1492598456f26711209e78ec8b23da390
Huan An Cheng <huanan_cheng@mymail.sutd.edu.sg>
2020-04-15 17:49:24 +0800

    added helper function to read record batch in utils.go

--- broker/utils.go
+++ broker/utils.go
@@ -5,6 +5,7 @@ import (
 	"AKFAK/proto/clientpb"
 	"AKFAK/proto/recordpb"
 	"fmt"
+	"errors"
 )
 
 // WriteRecordBatchToLocal is a helper function for Produce request handler to save the RecordBatch to the local log file
@@ -19,6 +20,20 @@ func WriteRecordBatchToLocal(topicName string, partitionID int, fileHandlerMappi
 		fileRecordHandler.WriteToFile(recordBatch)
 	}
 }
+
+// ReadRecordBatchFromLocal is a helper function for Consume request handler to read the Record from local log file
+func ReadRecordBatchFromLocal(topicName string, partitionID int, fileHandlerMapping map[int]*recordpb.FileRecord) (*recordpb.RecordBatch, error) {
+	fHandler, exist := fileHandlerMapping[partitionID]
+	if exist {
+		recordBatch, err := fHandler.ReadNextRecordBatch()
+		if err != nil {
+			return nil, err
+		}
+		return recordBatch, nil
+	}
+	return nil, errors.New("No such file")
+}
+
 
 // CleanupProducerResource help to clean up the Producer resources
 func CleanupProducerResource(replicaConn map[int]clientpb.ClientService_ProduceClient, fileHandlerMapping map[int]*recordpb.FileRecord) {
