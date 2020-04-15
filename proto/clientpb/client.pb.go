ec951188158b1e0016840deaf08eccadb1f0eb27
Huan An Cheng <huanan_cheng@mymail.sutd.edu.sg>
2020-04-15 17:09:58 +0800

    added rpc and messages for consumer

--- proto/clientpb/client.pb.go
+++ proto/clientpb/client.pb.go
@@ -4,6 +4,7 @@
 package clientpb
 
 import (
+	consumepb "AKFAK/proto/consumepb"
 	metadatapb "AKFAK/proto/metadatapb"
 	producepb "AKFAK/proto/producepb"
 	context "context"
@@ -31,19 +32,23 @@ func init() {
 }
 
 var fileDescriptor_e0b4fa04edbad8e6 = []byte{
-	// 188 bytes of a gzipped FileDescriptorProto
+	// 250 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x2e, 0x28, 0xca, 0x2f,
 	0xc9, 0xd7, 0x4f, 0xce, 0xc9, 0x4c, 0xcd, 0x2b, 0x29, 0x48, 0x82, 0x32, 0xf4, 0xc0, 0xa2, 0x42,
 	0x7c, 0x60, 0x4a, 0x0f, 0x26, 0x29, 0x25, 0x0b, 0x51, 0x5c, 0x50, 0x94, 0x9f, 0x52, 0x9a, 0x9c,
 	0x5a, 0x90, 0x04, 0x63, 0x41, 0x94, 0x4b, 0xc9, 0x43, 0xa4, 0x73, 0x53, 0x4b, 0x12, 0x53, 0x12,
-	0x4b, 0x12, 0x0b, 0x92, 0xe0, 0x4c, 0x88, 0x02, 0xa3, 0x7d, 0x8c, 0x5c, 0xbc, 0xce, 0x60, 0xc3,
-	0x82, 0x53, 0x8b, 0xca, 0x32, 0x93, 0x53, 0x85, 0x82, 0xb8, 0xd8, 0x03, 0x20, 0x66, 0x08, 0xc9,
-	0x43, 0x14, 0xe9, 0xc1, 0x4d, 0xd7, 0x83, 0xca, 0x04, 0xa5, 0x16, 0x96, 0xa6, 0x16, 0x97, 0x48,
-	0x29, 0xe0, 0x56, 0x50, 0x5c, 0x90, 0x9f, 0x57, 0x9c, 0xaa, 0xc4, 0xa0, 0xc1, 0x68, 0xc0, 0x28,
-	0x14, 0xc9, 0xc5, 0x17, 0x9e, 0x98, 0x59, 0xe2, 0x9f, 0xe7, 0x0b, 0xb5, 0x5d, 0x48, 0x11, 0xaa,
-	0x13, 0xe1, 0x32, 0x3d, 0x98, 0x1c, 0xcc, 0x70, 0x25, 0x7c, 0x4a, 0x60, 0xc6, 0x3b, 0x89, 0x45,
-	0x89, 0x38, 0x7a, 0xbb, 0x39, 0x7a, 0xeb, 0xa3, 0x86, 0x5a, 0x12, 0x1b, 0x98, 0x6f, 0x0c, 0x08,
-	0x00, 0x00, 0xff, 0xff, 0xb8, 0x8f, 0x78, 0x85, 0x4e, 0x01, 0x00, 0x00,
+	0x4b, 0x12, 0x0b, 0x92, 0xe0, 0x4c, 0xa8, 0x02, 0xa8, 0xfe, 0xe4, 0xfc, 0xbc, 0xe2, 0xd2, 0x5c,
+	0x90, 0x7e, 0x28, 0x0b, 0x22, 0x6d, 0xf4, 0x8e, 0x89, 0x8b, 0xd7, 0x19, 0x6c, 0x57, 0x70, 0x6a,
+	0x51, 0x59, 0x66, 0x72, 0xaa, 0x50, 0x10, 0x17, 0x7b, 0x00, 0xc4, 0x0a, 0x21, 0x79, 0x88, 0x22,
+	0x3d, 0xb8, 0xe5, 0x7a, 0x50, 0x99, 0xa0, 0xd4, 0xc2, 0xd2, 0xd4, 0xe2, 0x12, 0x29, 0x05, 0xdc,
+	0x0a, 0x8a, 0x0b, 0xf2, 0xf3, 0x8a, 0x53, 0x95, 0x18, 0x34, 0x18, 0x0d, 0x18, 0x85, 0x22, 0xb9,
+	0xf8, 0xc2, 0x13, 0x33, 0x4b, 0xfc, 0xf3, 0x7c, 0xa1, 0x8e, 0x13, 0x52, 0x84, 0xea, 0x44, 0x38,
+	0x5c, 0x0f, 0x26, 0x07, 0x33, 0x5c, 0x09, 0x9f, 0x12, 0x98, 0xf1, 0x20, 0xe7, 0x3a, 0x43, 0x7c,
+	0x04, 0x77, 0x2e, 0xdc, 0xaf, 0x7a, 0x50, 0x19, 0x74, 0xe7, 0x62, 0x51, 0x80, 0xe2, 0xdc, 0x28,
+	0x2e, 0x6e, 0xf7, 0xd4, 0x92, 0xa0, 0xd4, 0x82, 0x9c, 0xcc, 0xe4, 0xc4, 0x62, 0x21, 0x65, 0x0c,
+	0x6d, 0x48, 0xb2, 0x30, 0xb3, 0x55, 0xf0, 0x2b, 0x82, 0x99, 0xef, 0x24, 0x16, 0x25, 0xe2, 0xe8,
+	0xed, 0xe6, 0xe8, 0xad, 0x8f, 0x9a, 0x08, 0x92, 0xd8, 0xc0, 0x7c, 0x63, 0x40, 0x00, 0x00, 0x00,
+	0xff, 0xff, 0x5c, 0xd2, 0xc5, 0x97, 0x1d, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -61,6 +66,9 @@ type ClientServiceClient interface {
 	// Producer RPC
 	Produce(ctx context.Context, opts ...grpc.CallOption) (ClientService_ProduceClient, error)
 	WaitOnMetadata(ctx context.Context, in *metadatapb.MetadataRequest, opts ...grpc.CallOption) (*metadatapb.MetadataResponse, error)
+	// Consumer RPC
+	Consume(ctx context.Context, opts ...grpc.CallOption) (ClientService_ConsumeClient, error)
+	GetReplicas(ctx context.Context, in *consumepb.GetReplicasRequest, opts ...grpc.CallOption) (*consumepb.GetReplicasResponse, error)
 }
 
 type clientServiceClient struct {
@@ -111,11 +119,54 @@ func (c *clientServiceClient) WaitOnMetadata(ctx context.Context, in *metadatapb
 	return out, nil
 }
 
+func (c *clientServiceClient) Consume(ctx context.Context, opts ...grpc.CallOption) (ClientService_ConsumeClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_ClientService_serviceDesc.Streams[1], "/proto.clientpb.ClientService/Consume", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &clientServiceConsumeClient{stream}
+	return x, nil
+}
+
+type ClientService_ConsumeClient interface {
+	Send(*consumepb.ConsumeRequest) error
+	Recv() (*consumepb.ConsumeResponse, error)
+	grpc.ClientStream
+}
+
+type clientServiceConsumeClient struct {
+	grpc.ClientStream
+}
+
+func (x *clientServiceConsumeClient) Send(m *consumepb.ConsumeRequest) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *clientServiceConsumeClient) Recv() (*consumepb.ConsumeResponse, error) {
+	m := new(consumepb.ConsumeResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (c *clientServiceClient) GetReplicas(ctx context.Context, in *consumepb.GetReplicasRequest, opts ...grpc.CallOption) (*consumepb.GetReplicasResponse, error) {
+	out := new(consumepb.GetReplicasResponse)
+	err := c.cc.Invoke(ctx, "/proto.clientpb.ClientService/GetReplicas", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ClientServiceServer is the server API for ClientService service.
 type ClientServiceServer interface {
 	// Producer RPC
 	Produce(ClientService_ProduceServer) error
 	WaitOnMetadata(context.Context, *metadatapb.MetadataRequest) (*metadatapb.MetadataResponse, error)
+	// Consumer RPC
+	Consume(ClientService_ConsumeServer) error
+	GetReplicas(context.Context, *consumepb.GetReplicasRequest) (*consumepb.GetReplicasResponse, error)
 }
 
 // UnimplementedClientServiceServer can be embedded to have forward compatible implementations.
@@ -127,6 +178,12 @@ func (*UnimplementedClientServiceServer) Produce(srv ClientService_ProduceServer
 }
 func (*UnimplementedClientServiceServer) WaitOnMetadata(ctx context.Context, req *metadatapb.MetadataRequest) (*metadatapb.MetadataResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method WaitOnMetadata not implemented")
+}
+func (*UnimplementedClientServiceServer) Consume(srv ClientService_ConsumeServer) error {
+	return status.Errorf(codes.Unimplemented, "method Consume not implemented")
+}
+func (*UnimplementedClientServiceServer) GetReplicas(ctx context.Context, req *consumepb.GetReplicasRequest) (*consumepb.GetReplicasResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetReplicas not implemented")
 }
 
 func RegisterClientServiceServer(s *grpc.Server, srv ClientServiceServer) {
@@ -177,6 +234,50 @@ func _ClientService_WaitOnMetadata_Handler(srv interface{}, ctx context.Context,
 	return interceptor(ctx, in, info, handler)
 }
 
+func _ClientService_Consume_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(ClientServiceServer).Consume(&clientServiceConsumeServer{stream})
+}
+
+type ClientService_ConsumeServer interface {
+	Send(*consumepb.ConsumeResponse) error
+	Recv() (*consumepb.ConsumeRequest, error)
+	grpc.ServerStream
+}
+
+type clientServiceConsumeServer struct {
+	grpc.ServerStream
+}
+
+func (x *clientServiceConsumeServer) Send(m *consumepb.ConsumeResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *clientServiceConsumeServer) Recv() (*consumepb.ConsumeRequest, error) {
+	m := new(consumepb.ConsumeRequest)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func _ClientService_GetReplicas_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(consumepb.GetReplicasRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ClientServiceServer).GetReplicas(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/proto.clientpb.ClientService/GetReplicas",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ClientServiceServer).GetReplicas(ctx, req.(*consumepb.GetReplicasRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _ClientService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "proto.clientpb.ClientService",
 	HandlerType: (*ClientServiceServer)(nil),
@@ -185,11 +286,21 @@ var _ClientService_serviceDesc = grpc.ServiceDesc{
 			MethodName: "WaitOnMetadata",
 			Handler:    _ClientService_WaitOnMetadata_Handler,
 		},
+		{
+			MethodName: "GetReplicas",
+			Handler:    _ClientService_GetReplicas_Handler,
+		},
 	},
 	Streams: []grpc.StreamDesc{
 		{
 			StreamName:    "Produce",
 			Handler:       _ClientService_Produce_Handler,
+			ServerStreams: true,
+			ClientStreams: true,
+		},
+		{
+			StreamName:    "Consume",
+			Handler:       _ClientService_Consume_Handler,
 			ServerStreams: true,
 			ClientStreams: true,
 		},
