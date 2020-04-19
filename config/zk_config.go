da0e666c235c84950671b7c1a943f85fec80c0ec
Huan An Cheng <huanan_cheng@mymail.sutd.edu.sg>
2020-04-19 13:19:15 +0800

    fixed spelling mistake

--- config/zk_config.go
+++ config/zk_config.go
@@ -3,7 +3,7 @@ package config
 // ZKConfig represent the zookeeper config JSON data
 type ZKConfig struct {
 	DataDir 		string `json:"dataDir"`
-	ConsumerDataDir	string `json:"consumerdataDir"`
+	ConsumerDataDir	string `json:"consumerDataDir"`
 	Host    		string `json:"host"`
 	Port    		int    `json:"port"`
 }
