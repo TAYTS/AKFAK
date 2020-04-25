3604253bfbf01c306827e9f625f81b4ace9ed8c9
huanan1 <huanan_cheng@mymail.sutd.edu.sg>
2020-04-26 00:34:13 +0800

    new consumer cli

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -14,42 +14,47 @@ func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	// get the user input for initialise the Producer
-	cgID := flag.Int(
-		"cg-id",
+	cID := flag.Int(
+		"id",
 		0,
-		"Consumer group ID (e.g. 1)")
+		"Consumer ID (e.g. 1)")
 	contactServer := flag.String(
 		"kafka-server",
 		"",
 		"Kafka server IP address and port number (e.g. 127.0.0.1:9092)")
+	partitionNum := flag.Int(
+		"partition",
+		0,
+		"Partition Num (eg. 1)"
+	)
 	topicPtr := flag.String(
-		"topics",
+		"topic",
 		"",
-		"Topics to pull the message from, separated by comma (e.g. topic1,topic2")
+		"Topic to pull the message from")
 
 	// print usage if user does not provide kafka server address and the topic
 	if len(os.Args) < 4 {
-		fmt.Println("usage: consumer -kafka-server <server_address:port> -topics <topic_names>")
+		fmt.Println("usage: consumer -kafka-server <server_address:port> -topic <topic_name> -part <partition>")
 		os.Exit(2)
 	}
 	flag.Parse()
 
-	log.Println("Initialising the Consumer Group...")
+	log.Println("Initialising the Consumer...")
 
 	// initialise the Consumer Group
-	cg := consumer.InitConsumerGroup(*cgID, *topicPtr, *contactServer)
+	c := consumer.InitConsumer(*cID, *topicPtr, *partitionNum, *contactServer)
 
 	// Wait for Ctrl-C to exit
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt)
 
 	// routine to pass user input to the Producer
-	go func(cg *consumer.ConsumerGroup) {
+	go func(c *consumer.ConsumerGroup) {
 		var inputStr string
 		for {
 			fmt.Println("Enter a topic to pull from")
 			fmt.Scanln(&inputStr) // wait for something to be entered
-			go cg.Consume(inputStr)
+			go c.Consume(inputStr)
 		}
 	}(cg)
 	<-ch
