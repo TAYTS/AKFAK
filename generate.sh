fb40ea2ad0b2afac07f5891bd955594e00726501
TAY TS <taytzushieh@gmail.com>
2020-04-26 21:07:27 +0800

    Remove used generate script

--- generate.sh
+++ generate.sh
@@ -12,4 +12,3 @@ protoc proto/zkmessagepb/*.proto --go_out=plugins=grpc:..
 protoc proto/zookeeperpb/*.proto --go_out=plugins=grpc:..
 protoc proto/clustermetadatapb/*.proto --go_out=plugins=grpc:..
 protoc proto/heartbeatspb/*.proto --go_out=plugins=grpc:..
-protoc proto/consumermetadatapb/consumer_metadata.proto --go_out=plugins=grpc:..
